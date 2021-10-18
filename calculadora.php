15427d7d8cd53ba52f3b4b23d93a9824f9ae687b
adan <Losky92@gmail.com>
2021-10-18 11:06:54 +0200

    cucamona2.0

--- calculadora.php
+++ calculadora.php
@@ -19,20 +19,21 @@ Button {
 <form>
 
 
-<form>
+<form  method="POST">   >
         <fieldset><br>
             <legend>Calculadora</legend>
         <label >Numero1</label>
-        <input type="text" name="Nombre"><br>
+        <input type="text"><br>
         <label>Numero2</label>
-      
-        
-         <input type="text" name="2ºApellido"><br><br><br>
-        <label>operaciones</label><br><br><br> 
+        <input type="text" ><br><br><br>
+        <label>operaciones:</label><br><br><br> 
+       
         <input type="Button" name="" value=" + " onclick="calcula('+')"><br> 
         <input type="Button" name="" value=" - " onclick="calcula('-')"><br>  
         <input type="Button" name="" value=" X " onclick="calcula('*')"><br>  
-        <input type="Button" name="" value=" / " onclick="calcula('/')"><br>  
+        <input type="Button" name="" value=" / " onclick="calcula('/')"><br>
+
+        <input type="Button" name="raiz cuadrada" value="raiz cuadrada"><br><br><br>   
         <input type="Button" name="" value=" % " onclick="calcula('/')"><br><br><br> 
         <input type="Button" name="" value="resolver"><br><br><br>
         <h3>Resultado</h3><br><br>
@@ -41,6 +42,9 @@ Button {
 </form>
 
 <?php
+
+$num1=5;
+$num2=2;
 
 function suma($num1,$num2){
 
@@ -52,24 +56,79 @@ function suma($num1,$num2){
 
 function resta($num1,$num2){
 
-    $res= $num1 + $num2;
+    $res= $num1 - $num2;
 
     print "El resultado es ".$res."<br>";
 }
 
 function multiplicacion($num1,$num2){
 
-     $res= $num1 + $num2;
+     $res= $num1 * $num2;
 
   print "El resultado es ".$res."<br>";
 }
 
 function division($num1,$num2){
 
-    $res= $num1 + $num2;
+    $res= $num1 / $num2;
 
     print "El resultado es ".$res."<br>";
+
 }
+function raizcuadrada($num1){
+
+    $res= sqrt($num1,);
+
+    print "El resultado es ".$res."<br>";
+
+}
+function Elevar_al_cuadrado($num2){
+
+        $res= pow(num2,2);
+        print "El resultado es ".$res."<br>";
+
+}
+function Elevar_al_cubo($num2,){
+
+    $res= pow(num2,3);
+    print "El resultado es ".$res."<br>";
+
+}
+function elevar($num1,$num2){
+
+   $res=pow($num1,$num2);
+
+    print "El resultado es ".$res."<br>";
+
+   
+
+   }
+
+   function fivo($fin){
+       $var1=0;
+       $var2=1;
+       $var3=0;
+       $ini=0;
+   
+
+       while ($ini <= $fin) {
+        #paso1
+           $var3=$var1+$var2;
+        #paso2
+            $var1=$var2;
+            $var2=$var3;
+        #paso3
+            $ini++;
+            echo $var3."<br>";
+            }
+    
+
+
+
+   }
+    fivo(5);
+
+    
 
 ?>
 
