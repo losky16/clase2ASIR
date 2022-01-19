c5aa92417be8360d1cfbf7a8dacbee240c95c3ab
adan <Losky92@gmail.com>
2022-01-19 10:38:10 +0100

    sdasd

--- Ejercicio_Aerolineas/Arrays.php
+++ Ejercicio_Aerolineas/Arrays.php
@@ -2,16 +2,16 @@
 
 $vueloDestino=array(
 
-    "Vueling-AE12"=>"Milan",
-    "Volotea-EA21"=>"Roma",
-    "Iberia-WE44"=>"Estabul",
-    "Ryanair-EW44"=>"Madrid",
-    "Vueling-ZK87"=>"Lima",
-    "Volotea-UA21"=>"Bilbao",
-    "Iberia-WH44"=>"Roma",
-    "Ryanair-ELOO"=>"Madrid",
-    "Vueling-AE12"=>"Roma",
-    "Volotea-EA21"=>"Roma"
+    array("Vueling-AE12"=>"Milan"),
+    array("Volotea-EA21"=>"Roma"),
+    array("Iberia-WE44"=>"Estabul"),
+    array("Ryanair-EW44"=>"Madrid"),
+    array("Vueling-ZK87"=>"Lima"),
+    array("Volotea-UA21"=>"Bilbao"),
+    array("Iberia-WH44"=>"Roma"),
+    array("Ryanair-ELOO"=>"Madrid"),
+    array ("Vueling-AE12"=>"Roma"),
+    array("Volotea-EA21"=>"Roma")
 
 
 
@@ -19,18 +19,21 @@ $vueloDestino=array(
 );
 print_r($vueloDestino);
 echo "<br/>";
+echo "<br/>";
+echo "<br/>";
+echo "<br/>";
 
 
 $VueloFabricante=array(
 
-    "Vueling-AE12"=>"Boering",
-    "Volotea-EA21"=>"Airbus",
-    "Iberia-WE44"=>"Airbus",
-    "Ryanair-EW44"=>"Boering",
-    "Vueling-ZK87"=>"Airbus",
-    "Volotea-UA21"=>"Boering",
-    "Iberia-WH44"=>"Boering",
-    "Ryanair-ELOO"=>"Airbus"
+    array("Vueling-AE12"=>"Boering"),
+    array("Volotea-EA21"=>"Airbus"),
+    array("Iberia-WE44"=>"Airbus"),
+    array("Ryanair-EW44"=>"Boering"),
+    array("Vueling-ZK87"=>"Airbus"),
+    array("Volotea-UA21"=>"Boering"),
+    array("Iberia-WH44"=>"Boering"),
+    array("Ryanair-ELOO"=>"Airbus")
     
 
 
@@ -41,41 +44,45 @@ $VueloFabricante=array(
 );
 print_r($VueloFabricante);
 echo "<br/>";
+echo "<br/>";
+echo "<br/>";
 
 $VueloDuracionViaje=array(
 
-    "Vueling-AE12"=>"120 minutos",
-    "Volotea-EA21"=>"100 minutos",
-    "Iberia-WE44"=>"340 minutos",
-    "Ryanair-EW44"=>"45 minutos",
-    "Vueling-ZK87"=>"600 minutos",
-    "Volotea-UA21"=>"65 minutos",
-    "Iberia-WH44"=>"90 minutos",
-    "Ryanair-ELOO"=>"50 minutos",
-    "Vueling-AE12"=>"115 minutos",
-    "Volotea-EA21"=>"100 minutos",
-    "Iberia-WE44"=>"85 minutos",
-    "Ryanair-EW44"=>"360 minutos"
+    array("Vueling-AE12"=>"120 minutos"),
+    array("Volotea-EA21"=>"100 minutos"),
+    array("Iberia-WE44"=>"340 minutos"),
+    array("Ryanair-EW44"=>"45 minutos"),
+    array("Vueling-ZK87"=>"600 minutos"),
+    array("Volotea-UA21"=>"65 minutos"),
+    array("Iberia-WH44"=>"90 minutos"),
+    array("Ryanair-ELOO"=>"50 minutos"),
+    array("Vueling-AE12"=>"115 minutos"),
+    array("Volotea-EA21"=>"100 minutos"),
+    array("Iberia-WE44"=>"85 minutos"),
+    array("Ryanair-EW44"=>"360 minutos")
 
 
 );
 print_r($VueloDuracionViaje);
+echo "<br/>";
+echo "<br/>";
 echo "<br/>";
 
 
 
 $VueloNumeroPasajeros=array(
 
-    "Vueling-AE12"=>"90 personas",
-    "Volotea-EA21"=>"75 personas",
-    "Iberia-WE44"=>"100 personas",
-    "Ryanair-EW44"=>"60 personas",
-    "Vueling-ZK87"=>"77 personas",
-    "Volotea-UA21"=>"93 personas",
-    "Iberia-WH44"=>"Roma",
-    "Ryanair-ELOO"=>"Madrid",
-    "Vueling-AE12"=>"Roma",
-    "Volotea-EA21"=>"Roma"
+    array("Vueling-AE12"=>"90 personas"),
+    array("Volotea-EA21"=>"75 personas"),
+    array("Iberia-WE44"=>"100 personas"),
+    array("Ryanair-EW44"=>"60 personas"),
+    array("Vueling-ZK87"=>"77 personas"),
+    array("Volotea-UA21"=>"93 personas"),
+    array("Iberia-WH44"=>"Roma"),
+    array("Ryanair-ELOO"=>"Madrid"),
+    array("Vueling-AE12"=>"Roma"),
+    array("Volotea-EA21"=>"Roma")
 
 
 
