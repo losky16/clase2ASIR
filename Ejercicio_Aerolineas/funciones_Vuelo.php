f855862040156187ac2e3d52f432ce399a05a91d
adan <Losky92@gmail.com>
2022-01-26 18:15:10 +0100

    mas o menos

--- Ejercicio_Aerolineas/funciones_Vuelo.php
+++ Ejercicio_Aerolineas/funciones_Vuelo.php
@@ -7,12 +7,12 @@ include "Arrays.php";
 
 
 
-
+//VueloFabricante
 foreach ($VueloFabricante as  $fabricantito =>$info){
     
 
     foreach ($info as $infosito =>$infu ) {
-        
+        /* Posible solucion 
       if ($fabricantito=="Airbus") {
           
         
@@ -22,13 +22,13 @@ foreach ($VueloFabricante as  $fabricantito =>$info){
       if ($fabricantito=='Borering') {
         
       }  
-
+      */
     }
     
 }
 echo "El fabricante del avion:".$infu."<br>";
 
-
+//VueloDestino
 foreach ($vueloDestino as $ciudad => $ciudadito) {
 
   foreach ($ciudadito as $ciudadela =>$palacete ) {
@@ -37,9 +37,18 @@ foreach ($vueloDestino as $ciudad => $ciudadito) {
   }
   
 }
-echo "El fabricante del avion:".$palacete;
+echo "Ultimo destino es:".$palacete."<br>";
 
+//Veulo
+foreach ($VueloDuracionViaje as $viaje => $viajecito) {
 
+  foreach ($viajecito as $viajectito =>$viajete ) {
+        
+    
+  }
+  
+}
+echo "Minutos del vuelo son:".$viajete."<br>";
 
 
 
