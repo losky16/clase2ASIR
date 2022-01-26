32656cd080de2360c779fcf0505035b5d7bdad99
adan <Losky92@gmail.com>
2022-01-26 19:52:24 +0100

    joeeee

--- Ejercicio_Aerolineas/Vuelos.php
+++ Ejercicio_Aerolineas/Vuelos.php
@@ -3,7 +3,7 @@ include ("Arrays.php");
 include ("funciones_Vuelo.php");
 #RECOGIDA DE TADOS
 
-$select=$_GET["operador"];
+
 
 
 
@@ -18,35 +18,6 @@ $select=$_GET["operador"];
 
 
 
-
-
-/*
-switch ($Fabricante) {
-    case 'Vueling-AE12':
-        echo "Boering";
-        break;
-    
-    case 'Volotea-EA21':
-        echo"Airbus";
-        break;
-    case 'Iberia-WE44':
-        echo "Airbus";
-    case 'Ryanair-EW44':
-        echo "Boering";
-    case 'Vueling-ZK87':
-        echo "Airbus";
-    case 'Volotea-UA21':
-        echo "Boering";
-    case 'Iberia-WH44':
-        echo "Boering";
-    case 'Ryanair-ELOO':
-        echo "Airbus";
-    
-    default:
-        
-        break;
-}
-*/
 
 
 
