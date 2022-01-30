6ee186a9d8b626ee9f95fb1814f39a451ce806ba
adan <Losky92@gmail.com>
2022-01-30 22:11:11 +0100

    vamosss!!!!

--- Ejercicio_Aerolineas/Vuelos.php
+++ Ejercicio_Aerolineas/Vuelos.php
@@ -11,28 +11,27 @@ buscarFabricante($vueloFabricante, $fabricante);
 mediaPasajeros($VueloNumeroPasajeros,$fabricante);
 totalMinutosVuelos($VueloDuracionViaje,$fabricante);
 
-
+/**
+ * funcion que calcula el total de minutos de  vuelo de una compañia
+ * @param VueloDuracionViaje contiene el array con los datos
+ * @param fabricante  compañia que a selecionado el usuario
+ */
 function totalMinutosVuelos($VueloDuracionViaje,$fabricante){
-        $totalTiempo=0;
-        echo "<br>";
-        
-
-        foreach ($VueloDuracionViaje as $DuracionViajecito) {
-                
-                
-                if ($fabricante==$DuracionViajecito["Nombre"]) {
-                    echo "<br>";
-                    
-                    $totalTiempo= $totalTiempo + $DuracionViajecito["Tiempo"];
-                    
-                }
-
-
-
-
-
+    // declaramos las variables a usar
+    $totalTiempo=0; // almacena el total del tiempo de vuelo
+    echo "<br>";
+    
+    //recorremos el array VueloDuracionViaje
+    foreach ($VueloDuracionViaje as $DuracionViajecito) {
+        //comprobamos que el nombre del vuelo sea el selecionado por el usuaario
+        if ($fabricante==$DuracionViajecito["Nombre"]) {
+            
+            //acumulamos el tiempo de vuelo en la variable
+            $totalTiempo= $totalTiempo + $DuracionViajecito["Tiempo"];
+            
         }
-        echo $totalTiempo;
+    }
+    echo "Minutos totales de vuelo: ". $totalTiempo ." minutos";
 }           
 
 
